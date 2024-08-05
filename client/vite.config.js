a3f44352dd208eb6dc9a4e041cf71f1c69cafb2f
LegendaryMinds <128437646+legendaryminds@users.noreply.github.com>
2024-08-05 14:00:15 -0700

    modified vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: "localhost",
+    port: 3000,
+    open: true, // Automatically open the app in the default browser
     proxy: {
-      "/api": "http://localhost",
+      "/api": {
+        target: "http://localhost:5000",
+        changeOrigin: true,
+        secure: false,
+      },
     },
   },
 });
